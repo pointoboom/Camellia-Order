b1b015e1b044ca83df40d19eeac28678cb856ca4
Boom <pointoboom@gmail.com>
2023-02-01 14:16:57 +0700

    feat:connect supabase

--- pages/api/order.js
+++ pages/api/order.js
@@ -1,10 +1,16 @@
-import { pool } from "../utils/db.js";
+// import { pool } from "../utils/db.js";
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://bippbkldwdozkbgkzlqo.supabase.co";
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
 export default async function handler(req, res) {
   if (req.method === "POST") {
   } else if (req.method === "GET") {
-    const result = await pool.query("select * from orders ");
+    const { data, error } = await supabase.from("orders").select();
+
     return res.json({
-      data: result.rows,
+      data: data,
     });
   }
 }
